71a1af6c41dfb9ac463d767f19e0bea9df6f9061
Kim Forest <89828714+Kim-kkong@users.noreply.github.com>
2022-02-12 01:11:19 +0900

    02.12

--- js/dino-common.js
+++ js/dino-common.js
@@ -1,8 +1,24 @@
-// common script
+/first day/
+
+// alert("hello world!");
 
 
 
+// console.log("hello, developer console!");
+// console.log("2");
+// console.log("3");
 
 
+// var variable_name;
+// variable_name = value;
 
 
+// var name=prompt("apple");
+// var name=prompt("apple")
+var a="I", b="am", c="genius";
+
+
+// console.log(name, "님 환영합니다!");
+console.log(a);
+console.log(b);
+console.log(c);
