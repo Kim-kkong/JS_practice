a3b15b9678674519fcd12b3dd8e0f8f81b01e935
Kim Forest <89828714+Kim-kkong@users.noreply.github.com>
2022-02-13 20:24:13 +0900

    20220213-3

--- js/dino-common.js
+++ js/dino-common.js
@@ -37,3 +37,22 @@ console.log(height_float, typeof(height_float));
  쌍따옴표는 \" 를 쓴다. 
  문자를 쓰고 싶을 때는 \\를 쓴다. 
  줄바꿈은 \n 을 쓴다. */
+
+
+
+ var empty_obj = {};
+ typeof(empty_obj);
+
+ var man = {name: "홍길동", age: 20, height: 180};
+
+ var book = {"title":"어린왕자"};
+ console.log("제목:", book.title);
+
+ /초기화 되지 않은 변수는 undifined 아예 변수가 지정되지 않았다는 뜻/
+
+ var null_var;
+]
+null_var= null;
+typeof(null_var);
+/이 경우 변수의  type은 object로 출력됨. null값으로 지정했다는 뜻/
+
